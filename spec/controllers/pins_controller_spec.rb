eeb3bc3710b30945c356ce468c277943116c1a15
Charles Beynon <eToThePiIPower@gmail.com>
2015-08-18 17:53:01 -0400

    Add PinsController and basic functionality/specs

---
+++ spec/controllers/pins_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe PinsController, type: :controller do
+  describe "GET #index" do
+    it "assigns @pins" do
+      pin = Pin.create(title: "Test", description: "123")
+      get :index
+      expect(assigns(:pins)).to eq([pin])
+    end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template(:index)
+    end
+
+    it "assigns @pins newest first" do
+      pin1 = Pin.create(title: "A", description: "123")
+      pin2 = Pin.create(title: "C", description: "123")
+      pin3 = Pin.create(title: "B", description: "123")
+      get :index
+      expect(assigns(:pins)).to eq([pin3, pin2, pin1])
+    end
+  end
+end
