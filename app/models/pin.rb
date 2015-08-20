f9369552ff4ddab22df9cbf2d7381cc98c07889b
Charles Beynon <eToThePiIPower@gmail.com>
2015-08-20 01:28:52 -0400

    Styling for Show page
    * Enable upload of large size images in addition to medium
      - 600x600
    * Add a panel style to show page
    * Styling in scss

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,6 +2,6 @@ class Pin < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :title, :description, :user_id
   
-  has_attached_file :image, styles: { medium: "300x300>" }
+  has_attached_file :image, styles: { medium: "300x300>", large: "600x600>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
