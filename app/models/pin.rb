2b3d40b9bd247ba3d5b6d5ce30f2719715e2275d
Charles Beynon <eToThePiIPower@gmail.com>
2015-08-19 22:45:30 -0400

    Add attachment image to pins

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,7 @@
 class Pin < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :title, :description, :user_id
+  
+  has_attached_file :image, style: { medium: "300x300" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
